9eed13592824ddb0282cc08cab1a117678b651ec
Franck59840 <depoorter.franck@gmail.com>
2022-12-21 16:53:55 +0100

    Create subscriber exception

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\EventSubscriber;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ExceptionEvent::class => 'onException',
+        ];
+    }
+}
