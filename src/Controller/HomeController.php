b309a550759a8dea20212ee098a4271593530587
Franck59840 <depoorter.franck@gmail.com>
2022-12-22 04:11:41 +0100

    add callApi

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,13 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\CallApiService;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CallApiService $callApiService): Response
     {
-        return $this->render('home/index.html.twig'
+        return $this->render('home/index.html.twig', [
+            'data' => $callApiService->callApiLatestMovies(),
+        ]
     );
     }
 }
